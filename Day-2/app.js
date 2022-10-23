9f70e60c6b403b56ef879d04ea09d95c04ed29be
Mateus-Bozzi <mateuspvz1@gmil.com>
2022-10-23 19:32:05 -0300

    Day Two

---
+++ Day-2/app.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual o seu nome?');
+let idade = parseInt(prompt('Qual sua idade?'));
+let tecnologia = prompt('Qual linguagem de programação você está estudando?');
+
+alert(`Olá ${nome} tudo bem?, vejo que está estudando ${tecnologia} aos ${idade} , muito interessante!!`)
